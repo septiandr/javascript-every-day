e35c419aee4af29884d171038269de534ca5701e
septiandr <sdwirisanggalih@gmail.com>
2025-11-19 08:40:15 +0700

    fizzbuzz

---
+++ fizzBuzz.js
@@ -0,0 +1,16 @@
+/*
+Jika kelipatan 3 → "Fizz"
+
+Jika kelipatan 5 → "Buzz"
+
+Jika kelipatan 3 & 5 → "FizzBuzz"
+*/
+const fizzbuzz = (n) => {
+  if (n % 3 === 0 && n % 5 === 0) {
+    return "FizzBuzz";
+  } else if (n % 3 === 0) {
+    return "Fizz";
+  } else if (n % 5 === 0) {
+    return "Buzz";
+  }
+};
