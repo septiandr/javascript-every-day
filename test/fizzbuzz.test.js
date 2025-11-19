cec42f8ad2544a56629db12a286a68ca6ee205fe
septiandr <sdwirisanggalih@gmail.com>
2025-11-19 10:13:44 +0700

    unit test

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -3,3 +3,26 @@ const { fizzbuzz } = require("../src/fizzBuzz.js");
 test("fizzbuzz function exists", () => {
   expect(typeof fizzbuzz).toBe("function");
 });
+
+test("fizbuzz kelipatan 3 return fizz", () => {
+  expect(fizzbuzz(6)).toBe("Fizz")
+})
+
+test("fizbuzz kelipatan 5 return Buzz", () => {
+  expect(fizzbuzz(10)).toBe("Buzz")
+})
+
+test("fizbuzz  15 return FizzBuzz", () => {
+  expect(fizzbuzz(15)).toBe("FizzBuzz")
+})
+
+test("negatif case: jika bukan kelipatan 5 harus return undefined", () => {
+  expect(fizzbuzz(7)).toBeUndefined();  // 7 bukan kelipatan 5
+});
+
+test("negatif case: jika bukan kelipatan 3 harus return undefined", () => {
+  expect(fizzbuzz(7)).toBeUndefined();  // 7 bukan kelipatan 3
+});
+
+
+
