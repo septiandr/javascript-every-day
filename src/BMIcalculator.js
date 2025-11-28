0704062137f0ed0d171c7a8f73b48a22ffb7f502
septiandr <sdwirisanggalih@gmail.com>
2025-11-28 22:26:43 +0700

    categori

--- src/BMIcalculator.js
+++ src/BMIcalculator.js
@@ -9,4 +9,17 @@ const rl = readline.createInterface({
 function hitungBMI(berat, tinggi){
     
     return berat / (tinggi * tinggi);
+    
+}
+
+function interpretasiBMI(bmi){
+    if(bmi < 18.5){
+        return "Kekurangan berat badan";
+    } else if(bmi < 25){
+        return "Normal";
+    } else if(bmi < 30){
+        return "Kelebihan berat badan";
+    } else {
+        return "Obesitas";
+    }
 }
