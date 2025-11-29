45f9006b4153ac21c3ac34c6d1e30c9d382de0af
septiandr <sdwirisanggalih@gmail.com>
2025-11-29 08:18:34 +0700

    test interpretasi

--- test/bmi.test.js
+++ test/bmi.test.js
@@ -3,3 +3,7 @@ const { hitungBMI, interpretasiBMI } = require('../src/BMIcalculator');
 test('hitungBMI', () => {
     expect(hitungBMI(70, 1.7)).toBe(24.22);
 });
+
+test('interpretasiBMI', () => {
+    expect(interpretasiBMI(24.22)).toBe("Normal");
+});
