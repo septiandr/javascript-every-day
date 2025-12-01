4cbd403349085f86d3e3610ea89ca177b38bcdb7
septiandr <sdwirisanggalih@gmail.com>
2025-12-01 10:42:06 +0700

    final

--- src/findUniqeNumber.js
+++ src/findUniqeNumber.js
@@ -1,20 +1,23 @@
 const findUniqeNumber = (arr) => {
-    const uniqeNumber = arr.reduce((acc, val) => {
-        console.log("🚀 ~ findUniqeNumber ~ val:", val)
-        console.log("🚀 ~ findUniqeNumber ~ acc:", acc)
-        const isExist = acc?.some(item => item === val)
+  let uniqeNumber = [];
+  let dobleNumber = [];
+  arr.forEach((val) => {
+    console.log(val);
+    const isExistUniqe = uniqeNumber?.some((item) => item === val);
+    const isExistDoble = dobleNumber?.some((item) => item === val);
+    if (isExistDoble) {
+      dobleNumber.push(val);
+    } else if (!isExistUniqe) {
+      uniqeNumber.push(val);
+    } else if (isExistUniqe) {
+      dobleNumber.push(val);
+      uniqeNumber = uniqeNumber.filter((item) => item !== val);
+    }
+  });
 
-        if(isExist){
-            acc?.pop(val)
-        }else{
-            acc?.push(val)
-        }
-        return acc
-    }, []);
-    return uniqeNumber
-}
+  return uniqeNumber;
+};
 
+console.log(findUniqeNumber([1, 1, 5, 8, 8, 3, 1, 3, 9]));
 
-console.log(findUniqeNumber([1,1,2,2,5,8]))
-
-module.exports = findUniqeNumber
+module.exports = findUniqeNumber;
