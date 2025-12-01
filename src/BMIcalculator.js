30fa0747e98080f481971d792507e2a560099fcd
septiandr <sdwirisanggalih@gmail.com>
2025-12-01 09:29:37 +0700

    bmi fix

--- src/BMIcalculator.js
+++ src/BMIcalculator.js
@@ -6,11 +6,11 @@ function hitungBMI(berat, tinggi) {
 function interpretasiBMI(bmi) {
   if (bmi < 18.5) {
     return "Kekurangan berat badan";
-  } else if (bmi < 25) {
+  } else if (bmi >= 18.5 && bmi < 25) {
     return "Normal";
-  } else if (bmi < 30) {
+  } else if (bmi >= 25 && bmi < 30) {
     return "Kelebihan berat badan";
-  } else {
+  } else if (bmi >= 30) {
     return "Obesitas";
   }
 }
