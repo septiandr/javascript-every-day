d50a0cda77660ba1455c2732fd75cfece3d64852
septiandr <sdwirisanggalih@gmail.com>
2025-12-03 11:02:34 +0700

    test character frequency

--- test/characterFrequency.test.js
+++ test/characterFrequency.test.js
@@ -3,3 +3,7 @@ const characterFrequency = require('../characterFrequency');
 test("available function", () => {
     expect(typeof characterFrequency).toBe("function")
 })
+
+test("character frequency", () => {
+    expect(characterFrequency("hello")).toEqual({ h: 1, e: 1, l: 2, o: 1 })
+})
