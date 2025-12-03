0665ffab51aed4a7834e98229f9c6f8978cd7438
septiandr <sdwirisanggalih@gmail.com>
2025-12-03 11:03:10 +0700

    test

--- test/characterFrequency.test.js
+++ test/characterFrequency.test.js
@@ -7,3 +7,7 @@ test("available function", () => {
 test("character frequency", () => {
     expect(characterFrequency("hello")).toEqual({ h: 1, e: 1, l: 2, o: 1 })
 })
+
+test("character frequency", () => {
+    expect(characterFrequency("hello world")).toEqual({ h: 1, e: 1, l: 3, o: 2, " ": 1, w: 1, r: 1, d: 1 })
+})
