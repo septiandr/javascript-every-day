6d801d2185f44952caeb601f55e432ac5543c200
septiandr <sdwirisanggalih@gmail.com>
2025-12-03 10:59:32 +0700

    frequency char

---
+++ src/characterFrequency.js
@@ -0,0 +1,14 @@
+function characterFrequency(str) {
+  const frequency = {};
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (frequency[char]) {
+      frequency[char]++;
+    } else {
+      frequency[char] = 1;
+    }
+  }
+
+  return frequency;
+}
